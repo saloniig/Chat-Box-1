ddb023cb4970c6bf80758e396fd8dd12ae3b1416
saloniig <saloniggoyal@gmail.com>
2020-10-01 16:10:12 +0530

    Password feature url has been added

--- Chat/urls.py
+++ Chat/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
+
+
 
 urlpatterns = [
+    path('reset-password', PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('reset-password/done', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>[0-9A-Za-z]+)-<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset-password/complete/',PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    path('', include('box.urls')),
+
 ]
